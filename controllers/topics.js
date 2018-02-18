98c5fb62d72ccdab8801f6069f372fbd5ccfff17
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-02-18 10:16:09 +0000

    added in comments for topic get all articles

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,5 +1,6 @@
 const Topic = require('../models/topics');
 const Articles = require('../models/articles');
+const Comments = require('../models/comments');
 
 
 
@@ -11,11 +12,35 @@ const getAllTopics = (req, res) => {
     });
 };
 
+// const getAllArticles = (req, res) => {
+//   return Articles.find({'belongs_to': req.params.topic_id}).lean()
+//     .then(articles => res.send({articles}))
+//     .catch(error => {
+//       res.status(500).send('Something broke!', error);
+//     });
+// };
+
+
+
 const getAllArticles = (req, res) => {
+  let articles;
   return Articles.find({'belongs_to': req.params.topic_id}).lean()
-    .then(articles => res.send({articles}))
+    .then(results => {
+      articles = results;
+      const commentsPromises = results.map( article =>  {
+        return Comments.find({ 'belongs_to': article._id}).lean();
+      }); 
+      return Promise.all(commentsPromises);
+    })
+    .then (comments => {
+      articles = articles.map((article, index)=>{
+        article.comments = comments[index].length;
+        return article;
+      });
+      return res.send ({articles});
+    })
     .catch(error => {
-      res.status(500).send('Something broke!', error);
+      return res.status(500).send('Something went wrong!', error);
     });
 };
 
